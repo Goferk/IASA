c6c11a9a421c2d39f77e9803502886ced666f088
Javier Echiburu <goferk95@gmail.com>
2018-12-03 14:31:03 -0300

    Add comments and implement initial sdk

--- solver.cpp
+++ solver.cpp
@@ -12,39 +12,106 @@ const int N = 3;
 int main() {
   string line;
   int number;
+  int aux;
   ifstream myfile;
   int numbers[N * N];
   int sdk[N * N * N * N];
   int fmly[N * N * N * N];
-  int sumfmly[N * N * N * N];
+  int sumfmly[N * N * N * N] = { };
+  int expectedSumfmly[N * N * N * N];
   int fil = 0;
   int col = 0;
 
+  // Seteo la cantidad de numeros posibles
   for (int i = 0; i < N * N; i++)
     numbers[i] = N * N;
 
+  // Abro el archivo de killer sudoku
   myfile.open ("ks.txt");
+
+  // Si se abrio correctamente, continuamos
   if (myfile.is_open()) {
+    // Leo cada linea
     while (getline (myfile, line)) {
+      // Armo el sudoku inicial
       if (fil < N * N) {
         col = 0;
+        // Recorro la linea caracter por caracter
         for (int i = 0; i < line.size(); i++) {
+          // Caso en el que el caracter es el numero de la familia
           if (isdigit(line[i]) && i > 1 && line[i - 1] == 'C') {
+            // Guardamos el primero digito
             number = line[i] - 48;
-            if (i + 1 < line.size() && isdigit(line[i + 1])) {
-              number = number * 10 + line[i + 1] - 48;
-              i++;
-            }
-            fmly[(fil * 9) + col] = number;
-            col++;
-          }
 
+            // Variable auxiliar para saber cuantos digitos llevamos
+            aux = 1;
+
+            // Recorremos hasta no encontrar mas digitos
+            for (int j = i; j + 1 < line.size() && isdigit(line[j + 1]); j++) {
+              // Unimos los digitos
+              number = number * pow(10, aux) + line[i + 1] - 48;
+
+              // Actualizamos el valor de i
+              i = j;
+
+              // Actualizamos la cantidad de digitos
+              aux++;
+            }
+
+            // Guardamos el numero de familia en la casilla
+            fmly[fil * N * N + col] = number;
+
+            // Guardamos la suma total de la familia actual
+            sumfmly[number - 1] += sdk[fil * N * N + col];
+
+            // Pasamos a la siguiente columna
+            col++;
+
+          // Caso en el que el caracter es el valor de la casilla y es entregado como pista
+          } else if (isdigit(line[i])) {
+            number = line[i] - 48;
+            aux = 1;
+
+            // Recorremos hasta no encontrar mas digitos
+            for (int j = i; j + 1 < line.size() && isdigit(line[j + 1]); j++) {
+              // Unimos los digitos
+              number = number * pow(10, aux) + line[i + 1] - 48;
+
+              // Actualizamos el valor de i
+              i = j;
+
+              // Actualizamos la cantidad de digitos
+              aux++;
+            }
+            // Actualizamos el valor de la casilla
+            sdk[(fil * N * N) + col] = number;
+
+            // Nos saltamos la 'C' de la casilla
+            i++;
+
+          // Caso en el que el caracter es el valor de la casilla y no es entregado como pista
+          } else if (line[i] == '.') {
+            // Actualizamos el valor de la casilla
+            sdk[fil * N * N + col] = 0;
+
+            // Nos saltamos la 'C' de la casilla
+            i++;
+          }
         }
       }
       std::cout << line << '\n';
       fil++;
     }
     myfile.close();
+
+/*
+    std::cout << "Familia" << '\n';
+    for (int i = 0; i < N*N*N*N; i++)
+      std::cout << fmly[i] << ", ";
+    std::cout << '\n' << "Tablero" << '\n';
+    for (int i = 0; i < N*N*N*N; i++)
+      std::cout << sdk[i] << ", ";
+*/
   } else
     std::cout << "Unable to open file";
 
